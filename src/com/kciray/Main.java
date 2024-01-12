495136de747790f881618e22094c52c3a44e82b4
IvanTest-31 <ivangl79@gmail.com>
2024-01-12 03:54:28 +0300

    Всё работает! Надо было добавить main - beanFactory.populateProperties(); "Теперь запустите ваш проект и убедитесь, что при вызове productService.getPromotionsService() вместо null возвращается наш бин."

--- src/com/kciray/Main.java
+++ src/com/kciray/Main.java
@@ -2,15 +2,19 @@ package com.kciray;
 
 import org.springframework.beans.factory.BeanFactory;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         BeanFactory beanFactory = new BeanFactory();
         beanFactory.instantiate("com.kciray");
+        beanFactory.populateProperties();//без этого не получается бин
 
         ProductService productService = (ProductService) beanFactory.getBean("productService");
         System.out.println(productService);//ProductService@612
         System.out.println();
-        System.out.println(productService.getPromotionsService());
+        System.out.println("productService.getPromotionsService():\n\t\t" + productService.getPromotionsService());
+
 
     }
 }
